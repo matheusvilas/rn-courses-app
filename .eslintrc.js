cd1ee6c6e980a21248f66344aa7e815f5f6431fb
MatheusVilas <matheus@vilasboas.dev>
2021-02-28 16:38:11 -0300

    fix: removing lint wrong rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,5 +7,9 @@ module.exports = {
   extends: ['@react-native-community', 'airbnb-typescript', 'prettier'],
   rules: {
     'react/prop-types': 0,
+    'no-unused-vars': 0,
+    '@typescript-eslint/no-unused-vars': [0],
+    'import/export': 0,
+    'class-methods-use-this': 0,
   },
 }
